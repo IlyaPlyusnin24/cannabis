980eaf8c3f7f89b67c76c2be4cbee6e0993ac226
Ilya Plyusnin <ilya.plyusnin@dps.nm.gov>
2024-03-15 16:45:14 -0600

    Set up routes for Home and Statute components
    Add router-outlet to the app component template
    
    Add withComponentInputBinding to routes provider
    
    Also provide the HttpClient

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { StatuteComponent } from './views/statute/statute.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'statute/:category', component: StatuteComponent },
+  { path: '**', component: HomeComponent },
+];
