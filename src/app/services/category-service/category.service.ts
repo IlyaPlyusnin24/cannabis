a9738c3169ad9732b7eec4230c3dc141568d5bb6
Ilya Plyusnin <ilya.plyusnin@dps.nm.gov>
2024-03-24 15:55:26 -0600

    Add headers to createCategories
    Add retrieveCategories method

--- src/app/services/category-service/category.service.ts
+++ src/app/services/category-service/category.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +8,13 @@ export class CategoryService {
   constructor(private http: HttpClient) {}
 
   createCategories(categories: any) {
-    return this.http.post('/api/cannabis', categories);
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    const body = JSON.stringify(categories);
+
+    return this.http.post('/api/Categories', body, { headers });
+  }
+
+  retrieveCategories(name: string) {
+    return this.http.get(`/api/Categories/${name}`);
   }
 }
